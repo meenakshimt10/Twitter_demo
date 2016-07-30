27a44d3b4f30ef6eaf4a23a64ab8d7702e1d0904
meenakshimt10 <meenakshi.mt10@gmail.com>
2016-07-30 10:34:02 -0700

    Initial Commit

---
+++ Twitter_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter_demo
+//
+//  Created by Meenakshi Muthuraman on 7/30/16.
+//  Copyright © 2016 Meenakshi Muthuraman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
